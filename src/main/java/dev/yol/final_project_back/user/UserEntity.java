6b6d39e2c017c4a99af8b560723a40e8a47a6be6
Yolanda <yolandaalfonsolopez@gmail.com>
2025-10-08 17:19:52 +0200

    add UserEntity

--- src/main/java/dev/yol/final_project_back/user/UserEntity.java
+++ src/main/java/dev/yol/final_project_back/user/UserEntity.java
@@ -1,5 +1,6 @@
 package dev.yol.final_project_back.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,5 +23,23 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id_user;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username; 
+    @Column(nullable = false, unique = true, length = 100)
+    private String email; 
+    @Column(nullable = false)
+    private String password;
+
+    //Relación con Profile
+    /* @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private ProfileEntity profile;
+
+    //Relación con Pacientes, que nos permite eliminarlos en cascada cuando borramos el user correspondiente,    
+    @OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private Set<PatientEntity> patients = new HashSet<>(); */
+
+
     
 }
