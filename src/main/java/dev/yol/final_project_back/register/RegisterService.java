02f1f9239de5b58c6f39875431b6d288a4058d0d
Yolanda <yolandaalfonsolopez@gmail.com>
2025-10-09 11:00:07 +0200

    add RegisterService

---
+++ src/main/java/dev/yol/final_project_back/register/RegisterService.java
@@ -0,0 +1,7 @@
+package dev.yol.final_project_back.register;
+
+import dev.yol.final_project_back.implementations.IRegisterService;
+
+public interface RegisterService extends IRegisterService<RegisterRequestDTO, RegisterResponseDTO>{
+
+}
