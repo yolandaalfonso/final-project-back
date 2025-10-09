fc3c53d6c081e63801c6521e2f2b748a8e6027cb
Yolanda <yolandaalfonsolopez@gmail.com>
2025-10-09 11:18:48 +0200

    FP-27 fix RegisterServiceImpl

--- src/main/java/dev/yol/final_project_back/register/InterfaceRegisterService.java
+++ src/main/java/dev/yol/final_project_back/register/InterfaceRegisterService.java
@@ -2,6 +2,6 @@ package dev.yol.final_project_back.register;
 
 import dev.yol.final_project_back.implementations.IRegisterService;
 
-public interface RegisterService extends IRegisterService<RegisterRequestDTO, RegisterResponseDTO>{
+public interface InterfaceRegisterService extends IRegisterService<RegisterRequestDTO, RegisterResponseDTO>{
 
 }
---
+++ src/main/java/dev/yol/final_project_back/register/RegisterServiceImpl.java
@@ -0,0 +1,5 @@
+package dev.yol.final_project_back.register;
+
+public class RegisterServiceImpl implements InterfaceRegisterService{
+    
+}
