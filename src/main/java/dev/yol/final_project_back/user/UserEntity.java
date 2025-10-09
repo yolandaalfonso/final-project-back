29c54a9a7ec64b2deebd063f195bb951fea3346d
Yolanda <yolandaalfonsolopez@gmail.com>
2025-10-09 15:29:15 +0200

    update: application.properties

--- src/main/java/dev/yol/final_project_back/user/UserEntity.java
+++ src/main/java/dev/yol/final_project_back/user/UserEntity.java
@@ -25,8 +25,6 @@ public class UserEntity {
     private Long id_user;
 
     @Column(nullable = false, unique = true, length = 100)
-    private String userName; 
-    @Column(nullable = false, unique = true, length = 100)
     private String email; 
     @Column(nullable = false)
     private String password;
