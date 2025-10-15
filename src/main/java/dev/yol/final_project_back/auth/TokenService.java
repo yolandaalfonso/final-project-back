5fe0bfd69e15430562022751fef15193a2397185
Yolanda <yolandaalfonsolopez@gmail.com>
2025-10-15 19:04:45 +0200

    add TokenService

---
+++ src/main/java/dev/yol/final_project_back/auth/TokenService.java
@@ -0,0 +1,17 @@
+package dev.yol.final_project_back.auth;
+
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+
+@Service
+public class TokenService {
+    private final JwtEncoder jwtEncoder;
+
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+}
